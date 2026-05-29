41122ae8cc089d146bcd604e1af8e527eea69324
Abdul sami <abdulsamimyousuf0@gmail.com>
2026-05-29 15:31:49 +0500

    u

--- src/Route.jsx
+++ src/Route.jsx
@@ -1,4 +1,43 @@
-// AppRouter.js
+// // AppRouter.js
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Navbar from "./components/Navbar";
+// import Footer from "./components/Footer";
+// import Home from "./pages/Home";    
+// import Products from "./pages/Products";
+// import ProductDetail from "./pages/ProductDetail";
+// import Contact from "./pages/Contact";
+// import Brands from "./pages/Brands";
+// import About from "./pages/About";
+// import Cart from "./pages/Cart";
+// import Checkout from "./pages/Chackout";  // Note: Keep as is or fix typo
+// import OrderSuccess from "./pages/OrderSuccess";
+// import TryOn from "./pages/TryOn";
+//  // Import the TryOn component
+
+// const AppRouter = () => {
+//     return (
+//         <Router>    
+//             <Navbar />
+//             <Routes>
+//                 <Route path="/" element={<Home />} />   
+//                 <Route path="/products" element={<Products />} />
+//                 <Route path="/product/:id" element={<ProductDetail />} />
+//                 <Route path="/contact" element={<Contact />} />
+//                 <Route path="/brands" element={<Brands />} />   
+//                 <Route path="/cart" element={<Cart />} />   
+//                 <Route path="/order-success" element={<OrderSuccess />} />   
+//                 <Route path="/checkout" element={<Checkout />} />   
+//                 <Route path="/about" element={<About />} />
+//                 <Route path="/tryon" element={<TryOn/>} />  {/* Add virtual try-on route */}
+//             </Routes>
+//             <Footer />
+//         </Router>
+//     );
+// }  
+
+// export default AppRouter;
+
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
@@ -10,10 +49,9 @@ import Contact from "./pages/Contact";
 import Brands from "./pages/Brands";
 import About from "./pages/About";
 import Cart from "./pages/Cart";
-import Checkout from "./pages/Chackout";  // Note: Keep as is or fix typo
+import Checkout from "./pages/Chackout";
 import OrderSuccess from "./pages/OrderSuccess";
 import TryOn from "./pages/TryOn";
- // Import the TryOn component
 
 const AppRouter = () => {
     return (
@@ -29,7 +67,8 @@ const AppRouter = () => {
                 <Route path="/order-success" element={<OrderSuccess />} />   
                 <Route path="/checkout" element={<Checkout />} />   
                 <Route path="/about" element={<About />} />
-                <Route path="/tryon" element={<TryOn/>} />  {/* Add virtual try-on route */}
+                {/* ✅ Virtual Try-On Route - Opens with pre-selected frame from product */}
+                <Route path="/tryon" element={<TryOn />} />
             </Routes>
             <Footer />
         </Router>
